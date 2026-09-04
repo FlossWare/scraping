8b0ffe13dad5a3ecf34e435ba7386ed30cb01cd7
Scot P. Floess <scot.floess@gmail.com>
2026-09-03 21:39:51 -0400

    fix: expose discovery capability explicitly

--- src/scraping/discovery/__init__.py
+++ src/scraping/discovery/__init__.py
@@ -1,6 +1,6 @@
-"""Acquisition primitives."""
+"""URI discovery primitives."""
 from .filesystem import filesystem_uris
 from .sitemap import sitemap_urls
-from .web import crawl, extract_links
+from .web import crawl, discover_links, extract_links
 
-__all__ = ["crawl", "extract_links", "filesystem_uris", "sitemap_urls"]
+__all__ = ["crawl", "discover_links", "extract_links", "filesystem_uris", "sitemap_urls"]
